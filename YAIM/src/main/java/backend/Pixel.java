23d06d897005fee2631c46b6bc73c9833f3c0628
Julian Benitez <jubenitez@itba.edu.ar>
2018-08-21 10:10:15 -0300

    Added Pixel Picker & Pixel

---
+++ YAIM/src/main/java/backend/Pixel.java
@@ -0,0 +1,20 @@
+package backend;
+
+public class Pixel {
+    private Integer x;
+    private Integer y;
+
+    public Integer getX() {
+        return x;
+    }
+
+    public Integer getY() {
+        return y;
+    }
+
+    public Pixel(Integer x, Integer y) {
+
+        this.x = x;
+        this.y = y;
+    }
+}
--- YAIM/src/main/java/frontend/PixelPickerControlPane.java
+++ YAIM/src/main/java/frontend/PixelPickerControlPane.java
@@ -1,8 +1,21 @@
 package frontend;
 
+import backend.Pixel;
+import backend.Utils;
 import javafx.scene.control.TextArea;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Future;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
 
 public class PixelPickerControlPane extends GridPane
 {
@@ -12,7 +25,25 @@ public class PixelPickerControlPane extends GridPane
     private Text xLabel = new Text("X:");
     private Text yLabel = new Text("Y:");
 
-    public PixelPickerControlPane(){
-//        -
+    private ImageView imageView;
+
+    private AtomicReference<Pixel> pixel;
+
+    public PixelPickerControlPane(ImageView imageView){
+        this.imageView = imageView;
+        pixel = new AtomicReference<>();
+    }
+
+    public Optional<Pixel> getChosenPixel(){
+        return Optional.ofNullable(pixel.get());
+    }
+
+    public void focus(ImageView imageView){
+        imageView.setOnMouseClicked(event -> {
+            Integer x = Utils.toInteger(event.getX());
+            Integer y = Utils.toInteger(event.getY());
+
+            pixel.set(new Pixel(x,y));
+        });
     }
 }
